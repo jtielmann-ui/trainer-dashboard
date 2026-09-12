300b2c62eb0631bc0b17be40a09eec89f48dea04
jtielmann-ui <jtielmann@njcraneexpert.com>
2026-09-12 08:26:53 -0400

    Remove sort parameters from API request

--- api/login.js
+++ api/login.js
@@ -75,7 +75,7 @@ module.exports = function(req, res) {
         'Content-Type': 'application/json',
         'Authorization': 'OAuth2 ' + token
       }, JSON.stringify({})),
-      makeRequest('api.podio.com', '/item/app/24013170/?limit=100&sort_by=created_on&sort_order=desc', 'GET', {
+      makeRequest('api.podio.com', '/item/app/24013170/?limit=100', 'GET', {
         'Authorization': 'OAuth2 ' + token
       })
     ]);
